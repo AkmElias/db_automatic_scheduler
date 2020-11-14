a149e57e85f8082cb3f69c870e80f6c9a857c0dd
AkmElias <akmelias11@gmail.com>
2020-11-15 01:36:55 +0600

    Authentication permission added in setting for django-res frame work

--- db_automatic_scheduler/settings.py
+++ db_automatic_scheduler/settings.py
@@ -139,7 +139,16 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
 # Static files (CSS, JavaScript, Images)
